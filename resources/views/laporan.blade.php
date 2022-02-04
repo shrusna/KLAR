812bd276792dc5e75ed6fbba977e731ccead812e
alsi <syahrulhusna@gmail.com>
2022-02-04 20:38:19 +0700

    benerin sidebar, nambahin chart

--- resources/views/laporan.blade.php
+++ resources/views/laporan.blade.php
@@ -5,7 +5,7 @@
     <?php include(app_path() . '/includes/config/navbar.php'); ?>
     <div class="container-fluid page-body-wrapper">
       <?php include(app_path() . '/includes/config/sidebar.php'); ?>
-      <?php include(app_path() . '/includes/chart/laporan.php'); ?>
+      <?php include(app_path() . '/includes/charts/chart_reports.php'); ?>
     </div>
   </div>
   <?php include(app_path() . '/includes/config/footer.php'); ?>
